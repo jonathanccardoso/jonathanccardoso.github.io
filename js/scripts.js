7cd746b7daacfe24c4c4ad594b3f24c73ba344b1
jonathanccardoso <jonathan99moura@hotmail.com>
2020-04-04 16:46:21 -0300

    Fix: resolve bug on nav and adds image on section

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,31 @@
 const labelNav = document.querySelector("label#labelNav");
 
-$(document).ready(function() {
-  $(window).scroll(function() {
+const linkNav = document.querySelector("nav > .nav-wrapper > ul > li > a");
+
+$(document).ready(function () {
+  var links = document.querySelectorAll("nav > .nav-wrapper > ul > li > a");
+  for (var x = 0; x < links.length; x++) {
+    links[x].onclick = function () {
+      labelNav.click();
+    };
+  }
+
+  $(window).scroll(function () {
     if ($(this).scrollTop() > 0) {
       labelNav.style.display = "none";
     } else {
       labelNav.style.display = "inline-block";
+    }
+  });
+
+  let i = true;
+  labelNav.addEventListener("click", () => {
+    if (i) {
+      document.querySelector("body").style.overflowY = "hidden";
+      i = false;
+    } else {
+      document.querySelector("body").style.overflowY = "initial";
+      i = true;
     }
   });
 });
